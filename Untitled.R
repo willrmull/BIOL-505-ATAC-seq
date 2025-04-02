f33428c874604a695bac81f2e3ac91ff31bf99fb
William Mullins <willrmullins96@gmail.com>
2025-04-02 18:39:56 -0500

    Added Packages
    Added packages that (may) be used for analyzing the sequences.

--- Untitled.R
+++ Untitled.R
@@ -1,1 +1,18 @@
-testing testing testing
+#Instal and load the required packages
+install.packages(Signac)
+library(Signac)
+install.packages(Seurat)
+library(Seurat)
+install.packages(GenomeInfoDb)
+library(GenomeInfoDb)
+install.packages(EnsDb.Hsapiens.v75)
+library(EnsDb.Hsapiens.v75)
+install.packages(ggplot2)
+library(ggplot2)
+install.packages(patchwork)
+library(patchwork)
+install.packages(Rsamtools)
+library(Rsamtools)
+
+#Setting seed to make results reproducible
+set.seed(1234)
