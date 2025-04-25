27696438e2f437da0a156bf0a3027b0cd60b9a7c
Calvin <calvinbelton9@ku.edu>
2025-04-25 11:13:24 -0500

    Update function _narrowpeak_to_bigbed() .py

--- function _narrowpeak_to_bigbed() .py
+++ function _narrowpeak_to_bigbed() .py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
+
 #This code takes a gzipped narrowPeak file and a chromosome size file, 
 #filters and formats them properly, then converts the peak file into a BigBed file for genome browser visualization.
 #Filter chromosome sizes
@@ -17,5 +17,5 @@ bedToBigBed -type=bed6+4 -as=narrowPeak.as  ${BIGBED}.tmp2 ${BIGBED}.chrsz.tmp $
 #Clean up temporary files
 rm -f ${BIGBED}.tmp ${BIGBED}.tmp2 ${BIGBED}.chrsz.tmp
 
-"""
+
 
