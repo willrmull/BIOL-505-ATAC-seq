958aaad4fc865b7b0a2014048234256778dd027b
carly123456789 <chopegagnon@gmail.com>
2025-05-02 10:55:08 -0500

    Update genome_builder_final.sh

--- scripts/genome_builder_final.sh
+++ scripts/genome_builder_final.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+#!!!!!!!!!!!!!!!!!NEEDS TO BE RUN IN AN ENVIRONMENT WITH SAMTOOLS AND BOWTIE2!!!!!!!!!!!!!!!!!!!!!!
 set -e
 
+# give user directions if they enter the wrong arugments
 if [[ "$#" -lt 2 ]]; then
   echo "Usage: ./build_genome_data.sh [GENOME] [DEST_DIR]"
   echo "Example: ./build_genome_data.sh hg38 /your/genome/data/path/hg38"
   exit 2
 fi
 
+# assign first argument as the genome and assign the second variable as an absolute path to the output directory (even if entered as a relative path)
 GENOME=$1
 DEST_DIR=$(cd $(dirname $2) && pwd -P)/$(basename $2)
 TSV=${DEST_DIR}/${GENOME}.tsv
 
+#if the output directory doesn't exist, create it
+#then navigate to the output directory
 mkdir -p ${DEST_DIR}
 cd ${DEST_DIR}
 
@@ -28,16 +33,19 @@ case "$GENOME" in
     REF_FA_URL="http://hgdownload.cse.ucsc.edu/goldenpath/hg19/encodeDCC/referenceSequences/male.hg19.fa.gz"
     REGEX_BFILT_PEAK_CHR_NAME="chr[\dXY]+"
     ;;
+#THIS IS OURS! It is a monarch butterfly genome from ncbi, the regex selects the regular numbered and Z chromosomes from the FASTA headers
   dp)
     REF_FA_URL="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/018/135/715/GCF_018135715.1_MEX_DaPlex/GCF_018135715.1_MEX_DaPlex_genomic.fna.gz"
-    REGEX_BFILT_PEAK_CHR_NAME="chromosome\s(\d+|Z)"
+    REGEX_BFILT_PEAK_CHR_NAME="chromosome\s(\d+|Z).*"
     ;;
+#if the genome isn't defined here, print an error 
   *)
     echo "Unsupported genome: $GENOME"
     exit 1
     ;;
 esac
 
+#Download the FASTA from the URL defined above
 echo "=== Downloading reference FASTA for ${GENOME}..."
 wget -c -O ref.fa.gz "$REF_FA_URL"
 gunzip -f ref.fa.gz
@@ -45,9 +53,11 @@ gunzip -f ref.fa.gz
 echo "=== Indexing FASTA with samtools..."
 samtools faidx ref.fa
 
+#creates a file listing each chromosome
 echo "=== Generating chrom sizes..."
 cut -f1,2 ref.fa.fai > ${GENOME}.chrom.sizes
 
+#create bowtie2 indexes
 echo "=== Building Bowtie2 index..."
 mkdir -p bowtie2_index
 cd bowtie2_index
@@ -55,6 +65,7 @@ ln -s ../ref.fa .
 bowtie2-build ref.fa ref.fa
 cd ..
 
+# creates a TSV file with the reference FASTA file, chromosome sizes file, regex filtering for chromosomes file, and indexed bowtie 2 file, alongside their respective paths
 echo "=== Creating TSV file..."
 cat > ${TSV} <<EOF
 genome_name\t${GENOME}
