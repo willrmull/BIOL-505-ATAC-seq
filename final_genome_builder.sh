9f726594b664c562484d70621e94317e09f89030
Calvin <calvinbelton9@ku.edu>
2025-05-06 19:46:01 -0500

    Update final_genome_builder.sh

--- final_genome_builder.sh
+++ final_genome_builder.sh
@@ -1,68 +1,51 @@
 #!/bin/bash
-set -e
 
-if [[ "$#" -lt 2 ]]; then
-  echo "Usage: ./build_genome_data.sh [GENOME] [DEST_DIR]"
-  echo "Example: ./build_genome_data.sh hg38 /your/genome/data/path/hg38"
-  exit 2
-fi
+# Exit immediately if a command exits with a non-zero status, if a variable is unset, or if a pipeline fails.
+set -euo pipefail
 
-GENOME=$1
-DEST_DIR=$(cd $(dirname $2) && pwd -P)/$(basename $2)
-TSV=${DEST_DIR}/${GENOME}.tsv
+# === User configuration ===
+GENOME="danaus_plexippus"  # Name of the genome, used for naming outputs
+REF_FA_URL="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/018/135/715/GCF_018135715.1_MEX_DaPlex/GCF_018135715.1_MEX_DaPlex_genomic.fna.gz"  # URL to download the reference FASTA file
+DEST_DIR="$(pwd)/genome_resources"  # Directory to store all generated files
+REGEX_BFILT_PEAK_CHR_NAME="chr.*"  # Regular expression for chromosome names (used by downstream tools)
+TSV="${DEST_DIR}/final_${GENOME}.tsv"  # Path to the final metadata TSV file
 
-mkdir -p ${DEST_DIR}
-cd ${DEST_DIR}
+# === Create destination directory ===
+mkdir -p "${DEST_DIR}"  # Make the destination directory if it doesn't exist
 
-# Set reference FASTA URL and chromosome filtering regex
-case "$GENOME" in
-  hg38)
-    REF_FA_URL="https://www.encodeproject.org/files/GRCh38_no_alt_analysis_set_GCA_000001405.15/@@download/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta.gz"
-    REGEX_BFILT_PEAK_CHR_NAME="chr[\dXY]+"
-    ;;
-  mm10)
-    REF_FA_URL="https://www.encodeproject.org/files/mm10_no_alt_analysis_set_ENCODE/@@download/mm10_no_alt_analysis_set_ENCODE.fasta.gz"
-    REGEX_BFILT_PEAK_CHR_NAME="chr[\dXY]+"
-    ;;
-  hg19)
-    REF_FA_URL="http://hgdownload.cse.ucsc.edu/goldenpath/hg19/encodeDCC/referenceSequences/male.hg19.fa.gz"
-    REGEX_BFILT_PEAK_CHR_NAME="chr[\dXY]+"
-    ;;
-  dp)
-    REF_FA_URL="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/018/135/715/GCF_018135715.1_MEX_DaPlex/GCF_018135715.1_MEX_DaPlex_genomic.fna.gz"
-    REGEX_BFILT_PEAK_CHR_NAME="chromosome\s(\d+|Z)"
-    ;;
-  *)
-    echo "Unsupported genome: $GENOME"
-    exit 1
-    ;;
-esac
+# === Download reference FASTA file ===
+echo "=== Downloading reference FASTA file..."
+curl -L ${REF_FA_URL} -o "${DEST_DIR}/ref.fa.gz"  # Download the gzipped reference genome FASTA file
 
-echo "=== Downloading reference FASTA for ${GENOME}..."
-wget -c -O ref.fa.gz "$REF_FA_URL"
-gunzip -f ref.fa.gz
+# === Unzip FASTA file ===
+echo "=== Unzipping reference FASTA file..."
+gunzip "${DEST_DIR}/ref.fa.gz"  # Uncompress the FASTA file
 
+# === Index FASTA ===
 echo "=== Indexing FASTA with samtools..."
-samtools faidx ref.fa
+samtools faidx "${DEST_DIR}/ref.fa"  # Create FASTA index using samtools
 
-echo "=== Generating chrom sizes..."
-cut -f1,2 ref.fa.fai > ${GENOME}.chrom.sizes
+# === Generate chromosome sizes ===
+echo "=== Generating chrom.sizes..."
+cut -f1,2 "${DEST_DIR}/ref.fa.fai" > "${DEST_DIR}/${GENOME}.chrom.sizes"  # Extract chromosome names and lengths
 
+# === Build Bowtie2 index ===
 echo "=== Building Bowtie2 index..."
-mkdir -p bowtie2_index
-cd bowtie2_index
-ln -s ../ref.fa .
-bowtie2-build ref.fa ref.fa
-cd ..
+mkdir -p "${DEST_DIR}/bowtie2_index"  # Create a directory for the Bowtie2 index
+cd "${DEST_DIR}/bowtie2_index"  # Change to that directory
+ln -sf ../ref.fa .  # Create a symbolic link to the FASTA file in the index directory
+bowtie2-build ref.fa ref.fa  # Build the Bowtie2 index
+cd ..  # Return to the previous directory
 
-echo -e "=== Creating TSV file..."
+# === Create genome TSV ===
+echo "=== Creating TSV file..."
 cat > "${TSV}" <<EOF
 genome_name	${GENOME}
 ref_fa	${DEST_DIR}/ref.fa
 chrsz	${DEST_DIR}/${GENOME}.chrom.sizes
 regex_bfilt_peak_chr_name	${REGEX_BFILT_PEAK_CHR_NAME}
-bowtie2_index	${DEST_DIR}/bowtie2_index
+bowtie2_idx_tar	${DEST_DIR}/bowtie2_index
 EOF
 
-echo "=== Done! TSV file written to ${TSV}"
+echo " Done! TSV file written to ${TSV}"  # Inform the user that the process is complete
 
