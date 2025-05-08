1eaffa6754f66da12cfea2cdded35ada9736eafd
Calvin <calvinbelton9@ku.edu>
2025-05-07 22:20:40 -0500

    Update detect_adapter.py

--- src/detect_adapter.py
+++ src/detect_adapter.py
@@ -1,84 +1,42 @@
-
-
 import sys
 import gzip
 
+# Global flag to control verbose output
 VERBOSE = False
 
+# Dictionary of common adapter sequences used in various library preparations
+# Keys are names; values are the byte-encoded sequences
 adapters = {
     'Illumina': b'AGATCGGAAGAGC',
     'Nextera ': b'CTGTCTCTTATA',
     'smallRNA': b'TGGAATTCTCGG'
 }
 
-
+# Function to open a file (gzip-compressed or not)
 def open_gz(fname):
+    # If file ends in '.gz', open with gzip, else open as binary
     return gzip.open(fname) if fname.endswith('.gz') else open(fname, 'rb')
 
-
+# Detect which adapter sequences are present in reads
 def detect_adapters_and_cnts(fname, max_n_lines=1000000):
+    # Initialize counter for each adapter type
     adapter_cnts = {
         'Illumina': 0,
         'Nextera ': 0,
         'smallRNA': 0
     }
 
+    # Open input FASTQ file (gzip or plain text)
     with open_gz(fname) as fp:
-        # read the first million sequences or to the end of the while -- whichever
-        # comes first, and then use the adapter for trimming which was found to
-        # occur most often
+        # Iterate through lines up to max_n_lines
         for seq_index, line in enumerate(fp):
             if seq_index >= max_n_lines:
                 break
+
+            # In a FASTQ file, every 4th line starting at 1 is a sequence line
             if seq_index % 4 != 1:
                 continue
-            for key in adapters:
-                if line.find(adapters[key]) > -1:
-                    adapter_cnts[key] += 1
 
-    observed_adapters = [
-        adapter for adapter, cnt in sorted(
-            adapter_cnts.items(), key=lambda x: -x[1])
-        if cnt > 0
-    ]
-    return observed_adapters, adapter_cnts, seq_index//4
+            # Check if any known adapter appears in this
 
 
-def detect_most_likely_adapter(fname):
-    observed_adapters, adapter_cnts, n_obs_adapters = detect_adapters_and_cnts(
-        fname)
-    if observed_adapters:
-        best_adapter = observed_adapters[0]
-    else:
-        best_adapter = ""
-
-    if VERBOSE:
-        print("\n\nAUTO-DETECTING ADAPTER TYPE\n===========================")
-        print("Attempting to auto-detect adapter type from the first 1 million sequences of the first file (>> {} <<)\n".format(
-            fname)
-        )
-        print("Found perfect matches for the following adapter sequences:")
-        print("Adapter type\tCount\tSequence\tSequences analysed\tPercentage")
-        for adapter in observed_adapters:
-            print("{}\t{}\t{}\t{}\t\t\t{:.2%}".format(
-                adapter,
-                adapter_cnts[adapter],
-                adapters[adapter].decode(),
-                n_obs_adapters,
-                adapter_cnts[adapter]/n_obs_adapters)
-            )
-    if best_adapter:
-        return adapters[best_adapter].decode()
-    else:
-        return ""
-
-
-def main():
-    global VERBOSE
-    VERBOSE = False
-    best_adapter = detect_most_likely_adapter(sys.argv[1])
-    print(best_adapter)
-
-
-if __name__ == '__main__':
-    main()
