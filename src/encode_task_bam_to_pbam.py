e4c3424eb793895f6b761fb353e9c93a159ef50f
Calvin <calvinbelton9@ku.edu>
2025-05-07 22:46:49 -0500

    Update encode_task_bam_to_pbam.py

--- src/encode_task_bam_to_pbam.py
+++ src/encode_task_bam_to_pbam.py
@@ -1,63 +1,64 @@
 #!/usr/bin/env python
 
-
-import sys
-import os
-import argparse
+# Import necessary libraries and modules
+import sys  # Provides access to system-specific parameters and functions
+import os  # For interacting with the operating system (file manipulation, paths, etc.)
+import argparse  # For handling command-line arguments
 from encode_lib_common import (
-    log,
-    ls_l,
-    mkdir_p,
-    rm_f,
+    log,  # Logging utility
+    ls_l,  # Function to list files in a directory
+    mkdir_p,  # Function to create directories if they do not exist
+    rm_f,  # Function to remove files
 )
 from encode_lib_genomic import (
-    bam_to_pbam,
+    bam_to_pbam,  # Function to convert BAM to PBAM format
 )
 
-
+# Function to parse command-line arguments
 def parse_arguments():
-    parser = argparse.ArgumentParser(prog='ENCODE bam to pbam',
-                                     description='')
-    parser.add_argument('bam', type=str,
-                        help='Path for BAM.')
-    parser.add_argument('--ref-fa', type=str,
-                        help='Path for reference fasta.')
-    parser.add_argument('--delete-original-bam', action='store_true',
-                        help='Delete original BAM after conversion.')
-    parser.add_argument('--out-dir', default='', type=str,
-                        help='Output directory.')
-    parser.add_argument('--log-level', default='INFO',
-                        choices=['NOTSET', 'DEBUG', 'INFO',
-                                 'WARNING', 'CRITICAL', 'ERROR',
-                                 'CRITICAL'],
-                        help='Log level')
+    # Create the argument parser
+    parser = argparse.ArgumentParser(prog='ENCODE bam to pbam', description='')
+
+    # Define the arguments the script will accept
+    parser.add_argument('bam', type=str, help='Path for BAM.')  # Path to the BAM file
+    parser.add_argument('--ref-fa', type=str, help='Path for reference fasta.')  # Path to the reference FASTA file
+    parser.add_argument('--delete-original-bam', action='store_true', help='Delete original BAM after conversion.')  # Flag to delete the original BAM file
+    parser.add_argument('--out-dir', default='', type=str, help='Output directory.')  # Directory to save the output files
+    parser.add_argument('--log-level', default='INFO', choices=['NOTSET', 'DEBUG', 'INFO', 'WARNING', 'CRITICAL', 'ERROR', 'CRITICAL'], help='Log level')  # Log level option
+
+    # Parse the arguments
     args = parser.parse_args()
 
+    # Set up logging based on the log level argument
     log.setLevel(args.log_level)
-    log.info(sys.argv)
-    return args
+    log.info(sys.argv)  # Log the command used to run the script
+    return args  # Return the parsed arguments
 
-
+# Main function that controls the script workflow
 def main():
-    # read params
+    # Parse the command-line arguments
     args = parse_arguments()
 
+    # Log the initialization process and create the output directory
     log.info('Initializing and making output directory...')
-    mkdir_p(args.out_dir)
+    mkdir_p(args.out_dir)  # Create the output directory if it doesn't already exist
 
-    # generate read length file
+    # Log the conversion process from BAM to PBAM and call the function to perform the conversion
     log.info('Converting BAM into pBAM...')
-    bam_to_pbam(args.bam, args.ref_fa, args.out_dir)
+    bam_to_pbam(args.bam, args.ref_fa, args.out_dir)  # Convert the BAM file to PBAM format
 
+    # If the flag to delete the original BAM file is set, delete it after conversion
     if args.delete_original_bam:
         log.info('Deleting original BAM...')
-        rm_f(args.bam)
+        rm_f(args.bam)  # Remove the original BAM file
 
+    # Log the list of all files in the output directory after the conversion
     log.info('List all files in output directory...')
-    ls_l(args.out_dir)
+    ls_l(args.out_dir)  # List the contents of the output directory
 
+    # Log that the process is complete
     log.info('All done.')
 
-
+# If this script is being run directly, execute the main function
 if __name__ == '__main__':
     main()
